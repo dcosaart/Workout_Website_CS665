3eb7c3b8341ab459cfecfc012a5a622ec46a370d
Dylan Cossaart <D927R827@wichita.edu>
2026-05-04 13:18:27 -0500

    more changes to plans

--- routes.py
+++ routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request, redirect
-from models import db, User, Plan, Workout
+from models import Exercise, WorkoutExercise, db, User, Plan, Workout
+from datetime import datetime
 
 main = Blueprint("main", __name__)
 
@@ -51,12 +52,52 @@ def get_workouts():
     workouts = Workout.query.all()
     return render_template("workouts.html", workouts=workouts)
 
-@main.route("/plans/viewplan/<int:plan_id>", methods=["GET"])
+@main.route("/plans/viewplan/<int:plan_id>", methods=["GET", "POST"])
 def view_plan(plan_id):
+    if request.method == "POST":
+        name = request.form["name"]
+        goal = request.form["goal"]
+        description = request.form["description"]
+        start_date_raw = request.form.get("start_date")
+        end_date_raw = request.form.get("end_date")
+    
+        if not name or not goal:
+            return "Invalid input ..."
+    
+        plan = Plan.query.get(plan_id)
+        if not plan:
+            return "Plan not found"
+    
+        plan.name = name
+        plan.goal = goal
+        plan.description = description
+        
+
+        start_date = None
+        end_date = None
+
+        if start_date_raw and start_date_raw!= 'No Start Date':
+            start_date = datetime.strptime(start_date_raw, "%Y-%m-%d").date()
+
+        if end_date_raw and end_date_raw != "No End Date":
+            end_date = datetime.strptime(end_date_raw, "%Y-%m-%d").date()
+
+
+        db.session.commit()
+  
+
     plan = Plan.query.get(plan_id)
+    workouts = (
+        db.session.query(Workout, WorkoutExercise, Exercise)
+        .join(WorkoutExercise, Workout.workout_id == WorkoutExercise.workout_id)
+        .join(Exercise, WorkoutExercise.exercise_id == Exercise.exercise_id)
+        .filter(Workout.plan_id == plan_id)
+        .all()
+    )
     if not plan:
         return "Plan not found"
-    return render_template("viewPlan.html", plan=plan)
+    return render_template("viewPlan.html", plan=plan, workouts=workouts)
+
 
 @main.route("/plans/add", methods=["GET", "POST"])
 def add_plan():
@@ -79,3 +120,12 @@ def add_plan():
 
     users = User.query.all()
     return render_template("create/addPlan.html", users=users)
+
+@main.route("/plans/delete/<int:plan_id>", methods=["POST"])
+def delete_plan(plan_id):
+    plan = Plan.query.get_or_404(plan_id)
+
+    db.session.delete(plan)
+    db.session.commit()
+
+    return redirect("/plans")
